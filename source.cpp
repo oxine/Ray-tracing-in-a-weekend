dc77204484a86caa3c32168dec3d44c185dd3fc9
zgxmy <zgxmy@126.com>
2020-01-08 17:21:54 +0800

    1

--- source.cpp
+++ source.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
-
+#include"vec3.h"
 using namespace std;
 
 int main() {
-	cout << "hw" << endl;
+	vec3 a(1, 2, 3);
+	vec3 b = -a;
+	vec3 c = +a;
 	system("pause");
+	a[1];
 	return 0;
 }
--- vec3.h
+++ vec3.h
@@ -3,10 +3,24 @@
 #include<iostream>
 
 class vec3 {
-	vec3() {};
+public:
+	float e[3];
+
+	vec3() {}
 	vec3(float x, float y, float z) { e[0] = x; e[1] = y; e[2] = z; }
-	inline float x() const { return e[0]; };
+
+	inline float x() const { return e[0]; }
+	inline float y() const { return e[1]; }
+	inline float z() const { return e[2]; }
+	inline float r() const { return e[0]; }
+	inline float g() const { return e[1]; }
+	inline float b() const { return e[2]; }
+
+	inline const vec3& operator+() { return *this; }
+	inline const vec3 operator-() { return vec3(-e[0], -e[1], -e[2]); }
+	//inline float operator[](int i) const { return e[i]; }
+	inline float& operator[](int i) { return e[i]; }
 	
 
-	float e[3];
+	
 };
